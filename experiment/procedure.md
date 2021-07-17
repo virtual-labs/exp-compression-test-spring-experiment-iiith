4a9712eb62bed23b0d06362dea4178e93342480f
Dinesh Garg <dinesh.garg@students.iiit.ac.in>
2021-07-17 18:35:51 +0530

    review

--- experiment/procedure.md
+++ experiment/procedure.md
@@ -1,15 +1,18 @@
-**Manual and Procedure for the experiment is as follows**
+**Procedure**
 
-Observation Table :
+1. Measure the diameter of the wire of the spring by using the micrometer.
+2. Measure the diameter of spring coils by using the vernier caliper
+3. Count the number of turns.
+4. Insert the spring in the spring testing machine and load the spring by a suitable weight and note
+ the corresponding axial deflection in tension or compression.
+5. Increase the load and take the corresponding axial deflection readings.
+6. Plot a curve between load and deflection. The shape of the curve gives the stiffness of the
+ Spring.
 
-| S.No  | Load   | Deflection  | Stiffness  |  Strain Energy    |
-|---|---|---|---|---|
-| 1  |   |   |   |   |
-| 2  |   |   |   |   |
-| 3  |   |   |   |   |
-| 4  |   |   |   |   |
-| 5  |   |   |   |   |
-| 6  |   |   |   |   |
-| 7  |   |   |   |   |
 
-<a href="docs/Exp-8-Compression-test-on-spring.pdf">read more</a>
+**Virtual Procedure**
+1. Start the Experiment by clicking the play button.
+2. Observe the simulation of the spring in the simulation window.
+3. The observation table provides the deflections and the stiffness of the spring based on the current parameters of the spring. 
+4. Change the parameters using the sliders and the changes would be reflected in the simulation as well as in the observation table.
+5. The Experiment allows you to pause/play/restart the simulation to view the values of different parameters at every stage of simulation.
--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,179 @@
-//Your JavaScript goes in here
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    const restartButton = document.getElementById('restart');
+    restartButton.addEventListener('click', function() { restart(); });
+
+    const playButton = document.getElementById('play');
+    playButton.addEventListener('click', function() { play(); });
+
+    const pauseButton = document.getElementById('pause');
+    pauseButton.addEventListener('click', function() { pause(); });
+
+    const slider = document.getElementById('speed');
+    const output = document.getElementById('demo_speed');
+    output.innerHTML = (slider.value) / 4;
+    slider.oninput = function() {
+        output.innerHTML = (this.value) / 4;
+        fps = originalfps * (output.innerHTML);
+        restart();
+    };
+
+    const sliders = ["turn", "widthString", "widthMaterial", "mass"];
+    sliders.forEach(function(elem, ind) {
+        const slider = document.getElementById(elem);
+        const output = document.getElementById("demo_" + elem);
+        output.innerHTML = slider.value; // Display the default slider value
+
+        slider.oninput = function() {
+            output.innerHTML = this.value;
+            params[elem] = Number(document.getElementById(elem).value);
+            restart();
+        };
+    });
+
+    function setAll() {
+        topStringLength = 200;
+        ratio = -1;
+    }
+
+    function restart() {
+        window.clearTimeout(tmHandle);
+        setAll();
+        play();
+    }
+
+    function play() {
+        tmHandle = window.setTimeout(draw, 1000 / fps);
+        pauseButton.removeAttribute("disabled");
+        restartButton.removeAttribute("disabled");
+        playButton.setAttribute("disabled", "true");
+    }
+
+    function pause() {
+        window.clearTimeout(tmHandle);
+        pauseButton.setAttribute("disabled", "true");
+        playButton.removeAttribute("disabled");
+    }
+
+    function drawObject(ctx, obj, color) {
+        ctx.save();
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(obj[0][0], obj[0][1]);
+
+        for (let i = 0; i < obj.length; ++i) {
+            const next = (i + 1) % obj.length;
+            ctx.lineTo(obj[next][0], obj[next][1]);
+        }
+
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+    }
+
+
+    const canvas = document.getElementById("main");
+    canvas.width = 900;
+    canvas.height = 1200;
+    canvas.style = "border:3px solid;";
+    const ctx = canvas.getContext("2d");
+
+    const originalfps = 20;
+    let fps = 20;
+
+    const midX = canvas.width / 2;
+    let topStringLength = 200;
+    let params = {
+        "mass": 150,
+        "turn": 4,
+        "widthString": 100,
+        "widthMaterial": 15,
+    };
+    let ratio = -1;
+    let flag = 0;
+    let box = [];
+    let end;
+    let tmHandle;
+
+    drawStatic();
+
+    function drawStatic() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.lineWidth = params["widthMaterial"];
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.font = "50px Arial";
+
+        ctx.moveTo(midX, 0);
+        ctx.beginPath();
+        let cn = 0;
+        let f = 0;
+        ctx.moveTo(midX, 0);
+        ctx.lineTo(midX, topStringLength);
+
+        for (let i = 0;; i++) {
+            let x = midX + params["widthString"] * Math.sin(i * Math.PI / 72 * ratio);
+            if (x >= midX && f === 0) {
+                f = 1;
+                cn++;
+            }
+            if (x < midX) {
+                f = 0;
+            }
+            if (cn === params["turn"]) {
+                end = i + topStringLength;
+                break;
+            }
+            ctx.lineTo(midX + params["widthString"] * Math.sin(i * Math.PI / 72 * ratio), topStringLength + i);
+        }
+        ctx.moveTo(midX, end);
+        ctx.lineTo(midX, end + 100);
+        ctx.stroke();
+
+        box = [
+            [midX + params["mass"] / 2, end + 100],
+            [midX + params["mass"] / 2, end + 100 + params["mass"]],
+            [midX - params["mass"] / 2, end + 100 + params["mass"]],
+            [midX - params["mass"] / 2, end + 100],
+        ];
+        drawObject(ctx, box, "black");
+        let deflection = 8 * 9.8 * params["mass"]; // 8gW
+        deflection *= (params["widthString"] ** 3); // D^3
+        deflection *= params["turn"]; // n
+        deflection /= (params["widthMaterial"] ** 4); // d^4
+
+        deflection /= 1000;
+        deflection = deflection.toPrecision(6);
+
+        let stiffness = (params["mass"] * params["mass"]) / deflection;
+        stiffness = stiffness.toPrecision(6);
+
+        document.getElementById("deflection").innerHTML = deflection.toString() + " mm";
+        document.getElementById("stiffness").innerHTML = stiffness.toString() + " mm";
+
+    }
+
+    function draw() {
+        drawStatic();
+        if (ratio <= -2) {
+            flag = 0;
+        } else if (ratio >= -1) {
+            flag = 1;
+        }
+        if (flag === 0) {
+            ratio += 0.1;
+            topStringLength += 10;
+
+        } else {
+            ratio -= 0.1;
+            topStringLength -= 10;
+        }
+        tmHandle = window.setTimeout(draw, 2000 / fps);
+    }
+
+});
